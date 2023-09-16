3a0f5b9125ab32be94a7327bdd4d559a23e9a5f8
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-16 14:49:05 +0900

    update

--- app/libraries/db.php
+++ app/libraries/db.php
@@ -1,10 +1,23 @@
 <?php
 require_once(__DIR__.'/../../vendor/autoload.php');
 
-$host = getenv('DB_HOST') ?: '127.0.0.1';
-$user = getenv('DB_USER') ?: '';
-$pass = getenv('DB_PASS') ?: '';
-$port = getenv('DB_PORT') ?: '27017';
+try {
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+	$dotenv->load();	
+} catch (Exception $e) {
+	echo json_encode($e);
+}
+
+
+$host = $_ENV['DB_HOST'] ?: '127.0.0.1';
+$user = $_ENV['DB_USER'] ?: '';
+$pass = $_ENV['DB_PASS'] ?: '';
+$port = $_ENV['DB_PORT'] ?: '27017';
+
+// $host = getenv('DB_HOST') ?: '127.0.0.1';
+// $user = getenv('DB_USER') ?: '';
+// $pass = getenv('DB_PASS') ?: '';
+// $port = getenv('DB_PORT') ?: '27017';
 
 $client = new MongoDB\Client("mongodb://". $user . ":" . $pass . "@" .  $host . ":27017");
 
