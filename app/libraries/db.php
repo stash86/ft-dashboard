73426b4217d36cdd0b87628dd278a1d0dc1ee0fa
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-16 15:20:05 +0900

    fix db file

--- app/libraries/db.php
+++ app/libraries/db.php
@@ -1,25 +1,15 @@
 <?php
 require_once(__DIR__.'/../../vendor/autoload.php');
 
-try {
-	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
-	$dotenv->load();	
-} catch (Exception $e) {
-	echo json_encode($e);
-}
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->load();	
 
 $host = $_ENV['DB_HOST'] ?: '127.0.0.1';
 $user = $_ENV['DB_USER'] ?: '';
 $pass = $_ENV['DB_PASS'] ?: '';
 $port = $_ENV['DB_PORT'] ?: '27017';
 
-// $host = getenv('DB_HOST') ?: '127.0.0.1';
-// $user = getenv('DB_USER') ?: '';
-// $pass = getenv('DB_PASS') ?: '';
-// $port = getenv('DB_PORT') ?: '27017';
-
-$client = new MongoDB\Client("mongodb://". $user . ":" . $pass . "@" .  $host . ":27017");
+$client = new MongoDB\Client("mongodb://". $user . ":" . $pass . "@" .  $host . ":" . $port);
 
 $databaseName = "myDatabase";
 $collectionName = "myCollection";
