129bf9a4938f9d0fe4decbf0595df928571345ee
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-16 12:19:48 +0900

    remove debugger

--- public/api.php
+++ public/api.php
@@ -1,10 +1,3 @@
 <?php
-// header("Content-Type:application/json");
-// require_once('../app/api.php');	
-echo getenv('API');
-// $enabled = getenv('API') ?: false;
-// if (boolval($enabled)) {
-// 	echo "true";
-// } else {
-// 	echo "false";
-// }
+header("Content-Type:application/json");
+require_once('../app/api.php');	
