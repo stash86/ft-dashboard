a42de088518a565ef72e58a86de4a2c9de2d6855
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-16 14:43:12 +0900

    debug

--- app/libraries/db.php
+++ app/libraries/db.php
@@ -1,18 +1,22 @@
 <?php
 require_once(__DIR__.'/../../vendor/autoload.php');
+try {
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+	$dotenv->load();	
+} catch (Exception $e) {
+	echo json_encode($e);
+}
 
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
-// $dotenv->load();
 
-// $host = $_ENV['DB_HOST'] ?: '127.0.0.1';
-// $user = $_ENV['DB_USER'] ?: '';
-// $pass = $_ENV['DB_PASS'] ?: '';
-// $port = $_ENV['DB_PORT'] ?: '27017';
+$host = $_ENV['DB_HOST'] ?: '127.0.0.1';
+$user = $_ENV['DB_USER'] ?: '';
+$pass = $_ENV['DB_PASS'] ?: '';
+$port = $_ENV['DB_PORT'] ?: '27017';
 
-$host = getenv('DB_HOST') ?: '127.0.0.1';
-$user = getenv('DB_USER') ?: '';
-$pass = getenv('DB_PASS') ?: '';
-$port = getenv('DB_PORT') ?: '27017';
+// $host = getenv('DB_HOST') ?: '127.0.0.1';
+// $user = getenv('DB_USER') ?: '';
+// $pass = getenv('DB_PASS') ?: '';
+// $port = getenv('DB_PORT') ?: '27017';
 
 $client = new MongoDB\Client("mongodb://". $user . ":" . $pass . "@" .  $host . ":27017");
 
