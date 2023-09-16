61ac5b3e998d55356808d4c7c009ebf5c03d5726
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-16 12:18:39 +0900

    debug

--- public/api.php
+++ public/api.php
@@ -1,10 +1,10 @@
 <?php
 // header("Content-Type:application/json");
 // require_once('../app/api.php');	
-
-$enabled = getenv('API') ?: false;
-if (boolval($enabled)) {
-	echo "true";
-} else {
-	echo "false";
-}
+echo getenv('API');
+// $enabled = getenv('API') ?: false;
+// if (boolval($enabled)) {
+// 	echo "true";
+// } else {
+// 	echo "false";
+// }
