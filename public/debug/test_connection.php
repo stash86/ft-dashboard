1bfbe1e083da6819e7a1b03e85c70e139c1a3c20
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-09-20 15:03:10 +0900

    add debug files

---
+++ public/debug/test_connection.php
@@ -0,0 +1,28 @@
+<?php
+require_once('../app/libraries/api.php');
+require_once('../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+$enabled = ($_ENV['DEBUG'] == 'true') ?: false;
+
+if($enabled) {
+    $path = '../bots.json';
+    $jsonString = file_get_contents($path);
+    $jsonData = json_decode($jsonString, true);
+    $current_time = new DateTime();
+
+    for($i=0; $i < count($jsonData); $i++){
+        echo "fetch datas from {$jsonData[$i]['ip_address']}<br/>";
+        $api = new Api($jsonData[$i]['ip_address'], $jsonData[$i]['username'], $jsonData[$i]['password']);
+        $profit = json_decode($api->profit(), true);
+        if (empty($profit)) {
+            echo "No data <br/><br/>";
+            continue;
+        }
+        $config = json_decode($api->show_config(), true);
+        $strategy_name = $config['strategy'];
+        echo "strategy name is {$strategy_name}<br/><br/>";
+    }
+}
