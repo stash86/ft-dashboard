45b20b93cce3ebb490d8717d728dc0ecdc53b7e7
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2023-11-21 17:42:47 +0900

    update fetch data

--- app/scripts/fetch_data.php
+++ app/scripts/fetch_data.php
@@ -15,28 +15,38 @@ $current_time = new DateTime();
 $start_0 = ($_ENV['START_0'] == 'true') ?: false;
 $bot_name_as_index = ($_ENV['BOT_NAME_AS_INDEX'] == 'true') ?: false;
 
-$document = [];
 $array_others=[];
 $array_strategy_name=[];
+
+$find_char = array("_prod","_");
+$replace_char = array("", " ");
+
 for($i=0; $i < count($jsonData); $i++){
 	echo "fetch datas from {$jsonData[$i]['ip_address']}\n";
 	$api = new Api($jsonData[$i]['ip_address'], $jsonData[$i]['username'], $jsonData[$i]['password']);
 	$profit = json_decode($api->profit(), true);
 	if (empty($profit)) {
+		$find = $collection->findOne(['_id'=>$jsonData[$i]['ip_address']]);
+		if(!is_null($find)) {
+			$deleteResult = $collection->deleteOne(['_id'=>$jsonData[$i]['ip_address']]);
+			$deleted = $deleteResult->getDeletedCount();
+			echo "Delete {$deleted} {$jsonData[$i]['ip_address']} data from db\n";
+		}
 		continue;
 	}
-	$profit['profit_open_coin'] = floatval($profit['profit_all_coin']) - floatval($profit['profit_closed_coin']);
-	$profit['days'] = days_interval($current_time, $profit['first_trade_date']);
-	$trade_count = $profit['closed_trade_count'];
-	$win_trades = $profit['winning_trades'];
-	// $win_rate = ($trade_count > 0) ? round($win_trades * 100 / $trade_count, 2) : 0;
-	// $profit['win_rate'] = $win_rate;
 	
-	$trades = json_decode($api->trades($trade_count), true)['trades'];
 	$balance = json_decode($api->balance(), true);
-
 	$start_balance = $balance['starting_capital'];
 
+	$profit['profit_open_coin'] = floatval($profit['profit_all_coin']) - floatval($profit['profit_closed_coin']);
+	$profit['profit_open_ratio'] = floatval($profit['profit_open_coin']) / floatval($start_balance);
+	$profit['days'] = days_interval($current_time, $profit['first_trade_date']);
+	$trade_count = $profit['closed_trade_count'];
+	
+	$profit['winrate'] = round($profit['winrate'] * 100, 2)?:(($trade_count > 0) ? round($profit['winning_trades'] * 100 / $trade_count, 2) : 0);
+
+	$trades = json_decode($api->trades($trade_count), true)['trades'];
+	
 	// Prepare profit chart data
 	$total_profit = 0;
     $total_profit_abs = 0;
@@ -66,7 +76,7 @@ for($i=0; $i < count($jsonData); $i++){
             	"is_open" => 0
             ];
     	}
-    	
+
         $close_profit = (($time_data[$j]['close_profit_abs'] * 100 / $start_balance) + $total_profit);
         $total_profit = $close_profit;
         $close_profit = round($close_profit, 3);
@@ -87,11 +97,8 @@ for($i=0; $i < count($jsonData); $i++){
 
 	if(count($status) > 0) {
         $open_trades_profit = 0;
-        for($j=0; $j < count($status); $j++){
-        	$open_rate = $status[$j]['open_rate'];
-            $open_trades_profit += ($status[$j]['profit_abs'] + $status[$j]['realized_profit']);
-            $status[$j]['min_profit'] = ($status[$j]['min_rate'] - $open_rate) / $open_rate;
-            $status[$j]['max_profit'] = ($status[$j]['max_rate'] - $open_rate) / $open_rate;
+        foreach ($status as $key => $value) {
+            $open_trades_profit += ($value['profit_abs'] + $value['realized_profit']);
         }
         $date_now = new DateTime();
         $open_profit_pct = $total_profit + ($open_trades_profit * 100 / $start_balance);
@@ -104,12 +111,20 @@ for($i=0; $i < count($jsonData); $i++){
 	if ($bot_name_as_index){
 		$strategy_name = $config['bot_name'];
 	}
-	
+
 	$performance = json_decode($api->performance(), true);
+
+	$entries = json_decode($api->entries(), true);
+	$exits = json_decode($api->exits(), true);
+	$mix_tags = json_decode($api->mix_tags(), true);
+
+	echo "Strategy name is {$strategy_name}\n";
 	
-	$document[] = [
+	$sanitized_name = str_replace($find_char, $replace_char, $strategy_name);
+	
+	$document = [
 		"_id"=> $jsonData[$i]['ip_address'],
-		"strategy"=> $strategy_name,
+		"strategy"=> $sanitized_name,
 		"strategy_version"=> $config['strategy_version'],
 		"profit"=> $profit,
 		"trades"=> $trades,
@@ -117,16 +132,35 @@ for($i=0; $i < count($jsonData); $i++){
 		"balance"=> $balance,
 		"config"=> $config,
 		"performance"=> $performance,
+		"entries"=> $entries,
+		"exits"=> $exits,
+		"mix_tags"=> $mix_tags,
 		"chart_profit_data"=> $chart_profit_data,
 		"fetched"=> $current_time->format('Y-m-d H:i:s')
 	];
 
-	$array_strategy_name[] = $strategy_name;
+	$find = $collection->findOne(['_id'=>$jsonData[$i]['ip_address']]);
+	if(is_null($find)) {
+		$insertDoc = $collection->insertOne($document);
+		$inserted = $insertDoc->getInsertedCount();
+		echo "Insert {$inserted} {$jsonData[$i]['ip_address']} data to db\n";
+	} else {
+		$operation_update = $collection->replaceOne(
+			[ '_id' => $jsonData[$i]['ip_address'] ],
+			$document
+		);
+
+		$replaced = $operation_update->getModifiedCount();
+
+		echo "Replaced {$replaced} {$jsonData[$i]['ip_address']} data on db\n";
+	}
+
+	$array_strategy_name[] = $sanitized_name;
 
 	$dur_sec = duration_string_to_seconds($profit['avg_duration']);
 	if($dur_sec > 0) {
 		$array_others[] = [
-			"strategy"=> $strategy_name,
+			"strategy"=> $sanitized_name,
 			"duration"=> $profit['avg_duration'],
 			"duration_sec"=> $dur_sec,
 			"trade_count"=> $profit['closed_trade_count'],
@@ -136,19 +170,47 @@ for($i=0; $i < count($jsonData); $i++){
 	}
 	
 }
-$document[] = [
+
+$document = [
 	"_id"=> 'others',
 	"data" => $array_others
 ];
 
+$find = $collection->findOne(['_id'=>'others']);
+if(is_null($find)) {
+	$insertDoc = $collection->insertOne($document);
+	$inserted = $insertDoc->getInsertedCount();
+	echo "Insert {$inserted} 'others' data to db\n";
+} else {
+	$operation_update = $collection->replaceOne(
+		[ '_id' => 'others' ],
+		$document
+	);
+
+	$replaced = $operation_update->getModifiedCount();
+
+	echo "Replaced {$replaced} 'others' data on db\n";
+}
+
 sort($array_strategy_name);
 
-$document[] = [
+$document = [
 	"_id"=> 'names',
 	"data" => $array_strategy_name
 ];
 
-$collection->drop();
-$operation_insert = $collection->insertMany($document);
+$find = $collection->findOne(['_id'=>'names']);
+if(is_null($find)) {
+	$insertDoc = $collection->insertOne($document);
+	$inserted = $insertDoc->getInsertedCount();
+	echo "Insert {$inserted} 'names' data to db\n";
+} else {
+	$operation_update = $collection->replaceOne(
+		[ '_id' => 'names' ],
+		$document
+	);
 
-printf("Inserted %d document(s)\n", $operation_insert->getInsertedCount());
+	$replaced = $operation_update->getModifiedCount();
+
+	echo "Replaced {$replaced} 'names' data on db\n";
+}
